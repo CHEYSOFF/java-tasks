3f6582c5f14a8be36e5f36e4e2902f9d189bb5ca
CHEYSOFF <slobberyblanket@gmail.com>
2023-03-29 21:31:15 +0300

    RepeatingCharacters all tests pass

--- src/main/java/ru/mail/polis/homework/objects/RepeatingCharacters.java
+++ src/main/java/ru/mail/polis/homework/objects/RepeatingCharacters.java
@@ -13,7 +13,41 @@ import java.util.Objects;
 public class RepeatingCharacters {
 
     public static Pair<Character, Integer> getMaxRepeatingCharacters(String str) {
-        return new Pair<>('s', 4);
+        if (str == null || str.equals("")) {
+            return null;
+        }
+        str = str.concat("_");
+        int[] alphabet = new int[26];
+        int[] firstInstance = new int[26];
+        for (int i = 0; i < alphabet.length; i++) {
+            alphabet[i] = 0;
+            firstInstance[i] = str.length();
+        }
+        int lastLetter = str.charAt(0) - 'a';
+        int ammount = 1;
+        for (int i = 1; i < str.length(); i++) {
+            int currentLetter = str.charAt(i) - 'a';
+            if (lastLetter == currentLetter) {
+                ammount++;
+            } else {
+                if (ammount > alphabet[lastLetter]) {
+                    alphabet[lastLetter] = ammount;
+                    firstInstance[lastLetter] = i - ammount;
+                }
+                ammount = 1;
+                lastLetter = currentLetter;
+            }
+
+
+        }
+        int maxLetter = 0;
+        for (int i = 0; i < alphabet.length; i++) {
+            if (alphabet[i] > alphabet[maxLetter] || (alphabet[i] == alphabet[maxLetter] && firstInstance[i] < firstInstance[maxLetter])) {
+                maxLetter = i;
+            }
+        }
+        char actualLetter = (char) ('a' + maxLetter);
+        return new Pair<>(actualLetter, alphabet[maxLetter]);
     }
 
     public static class Pair<T, V> {
